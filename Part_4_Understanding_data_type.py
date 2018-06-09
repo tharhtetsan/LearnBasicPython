0cd98b5c94b5921f01cd89a611167a52dbf8ae0c
tharhtetsan <tharhtet1234@gmail.com>
2018-06-09 12:03:17 +0630

    part4

---
+++ Part_4_Understanding_data_type.py
@@ -0,0 +1,32 @@
+'''
+string
+numbers
+list
+tuple
+dictionary
+'''
+
+
+#String
+name = 'mg mg'
+print("My name is : ",name)
+
+#numbers
+age = 20
+print("my age is ",age)
+
+
+#List
+arl = ["mg mg ",'U ba ',20]
+print(arl[2])
+
+
+#Tuple
+(x,y,z) = 20,30,40
+print(x)
+
+
+#Dictionary
+dic = {"no1" : 90,"myname":"kyaw kyaw",1:"youtube"}
+print(dic)
+print(dic[1])
