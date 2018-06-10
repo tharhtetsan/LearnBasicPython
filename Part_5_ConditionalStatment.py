7fc3905bf5336a8a5b2625400bf181fa661d2ef7
tharhtetsan <tharhtet1234@gmail.com>
2018-06-10 22:46:47 +0630

    10.6.2018

---
+++ Part_5_ConditionalStatment.py
@@ -0,0 +1,44 @@
+'''
+if
+if else
+if else if
+nest if
+'''
+
+
+num1 = 30
+num2 = 30
+
+
+
+if num1 > num2 :
+    print("num1 is greater")
+else :
+    print("num2 is greater")
+
+print(num1>num2)
+
+'''
+ > < =
+ Conparable Operator
+'''
+if num1 == num2:
+    print("They are equal")
+
+
+mark = 82
+if mark > 80:
+    print("A")
+elif mark >60:
+    print("B")
+elif mark > 40:
+    print("C")
+else :
+    print("D")
+
+age = 18
+if mark > 80 :
+    if age >= 20:
+        print('Success')
+    elif age < 20:
+        print('Great')
